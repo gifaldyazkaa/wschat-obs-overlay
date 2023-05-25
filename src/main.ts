3e89d7ef6c69ef46268992fa59303322ed8d3d1b
Gifaldy Azka <falcxxdev@pm.me>
2023-05-25 20:19:50 +0700

    chore(router): setup router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router';
 import { createApp } from 'vue';
 import './assets/css/tailwind.css';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: App,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+createApp(App).use(router).mount('#app');
